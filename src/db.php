699f544bbb19b52154818134ad803b72f70cafcc
mandragore <github@guilla.me>
2025-12-27 10:16:12 +0100

    fix creds

--- src/db.php
+++ src/db.php
@@ -7,6 +7,5 @@ $dbname = getenv('DB_NAME');
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: {$conn->connect_error}");
 }
-?>
